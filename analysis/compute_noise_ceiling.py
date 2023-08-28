e654b1326daa0fcfa66cb431a66475c37c7fd513
arcaro-lab-general <arcaroresearch@gmail.com>
2023-08-28 17:32:22 -0400

    added noise ceiling analyses

---
+++ analysis/compute_noise_ceiling.py
@@ -0,0 +1,181 @@
+'''
+compute noise cieling
+'''
+
+git_dir = '/mnt/c/Users/ArcaroLab/Desktop/git_repos/dhcp'
+
+import os
+import sys
+#add git_dir to path
+sys.path.append(git_dir)
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import dhcp_params as params
+
+import pdb
+
+age_groups= ['infant', 'adult']
+atlas = 'wang'
+
+#load roi labels
+atlas_name, roi_labels = params.load_roi_info(atlas)
+
+#add networks to roi labels
+roi_labels['network'] = ['EVC']*6 + ['ventral']*5 + ['lateral']*6 + ['dorsal']*7
+
+networks = ['EVC', 'ventral', 'lateral', 'dorsal']
+
+def compute_noise_ceiling(all_rdms):
+
+    '''
+    Compute the noise ceiling by using leave one out cross validation
+    
+    '''
+    #create list of indices with length of number of subjects
+    sub_idx = np.arange(len(all_rdms.columns))
+
+    #create empty list to store noise ceiling values
+    noise_ceiling = []
+
+    #loop through indices and remove one subject at a time
+    #calcualte median rdm for remaining subjects
+    #compute correlation between median rdm and removed subject
+    for idx in sub_idx:
+        
+        #remove subject col from df
+        sub_removed = all_rdms.drop(columns = idx)
+        
+        #compute median rdm
+        median_rdm = np.median(sub_removed, axis = 1)
+
+        #extract upper triangle of removed subject
+        curr_sub = all_rdms[idx].values
+        
+        #compute correlation between median rdm and removed subject
+        r = np.corrcoef(median_rdm, curr_sub)[0,1]
+
+        #append to noise_ceiling
+        noise_ceiling.append(r)
+
+    return noise_ceiling
+
+
+
+
+
+def preprocess_data(group, summary_type, all_rois, all_networks):
+    '''
+    Organize data for noise ceiling analysis
+    '''
+    
+
+    raw_data_dir, raw_anat_dir, raw_func_dir, out_dir, anat_suf, func_suf = params.load_group_params(group)
+    #load individual infant data
+    all_rdms = np.load(f'{out_dir}/derivatives/fc_matrix/{group}_{atlas}_{summary_type}.npy')
+
+
+
+
+    all_sub_df = pd.DataFrame()
+    for i in range(all_rdms.shape[0]):
+        curr_fc = all_rdms[i,:,:]
+        #set diagonal to nan only if analysis type i not cross-hemi
+        if summary_type != 'cross_hemi':
+            np.fill_diagonal(curr_fc, np.nan)
+
+        #convert to pandas dataframe and melt
+        curr_sub = pd.DataFrame(curr_fc, index = all_rois, columns = all_rois)
+        curr_sub = pd.melt(curr_sub, var_name ='roi',value_name='corr')
+        curr_sub = curr_sub.dropna()
+
+        #add curr_sub to all_sub_df
+        all_sub_df[i] = curr_sub['corr']
+
+    all_sub_df['roi'] = curr_sub['roi']
+
+
+    #add network column
+    #add columns for network
+    for roi in all_sub_df['roi']:
+        #find index of roi in all_labels
+        idx = all_rois.index(roi)
+        #get network
+        network = all_networks[idx]
+        #add to dataframe
+        all_sub_df.loc[all_sub_df['roi'] == roi, 'network'] = network
+
+    #extract noise ceiling for each roi
+    roi_df = pd.DataFrame(columns = all_rois)
+    for roi in all_rois:
+
+        #extract roi fc
+        roi_fc = all_sub_df[all_sub_df['roi'] == roi]
+
+        #extract only subject columns
+        roi_fc = roi_fc.drop(columns = ['roi','network'])
+        
+        #compute noise ceiling
+        noise_ceilings = compute_noise_ceiling(roi_fc)
+        #add to all_sub_df
+        roi_df[roi] = noise_ceilings
+
+    #save
+    roi_df.to_csv(f'{params.results_dir}/noise_ceilings/{group}_{atlas}_{summary_type}_roi_noise_ceilings.csv', index=False)
+
+    #Extract noise ceiling for each network
+    network_df = pd.DataFrame(columns = networks)
+    #First compute overall noise ceiling
+
+    network_fc = all_sub_df.drop(columns = ['roi','network'])
+    noise_ceilings = compute_noise_ceiling(network_fc)
+    network_df['overall'] = noise_ceilings
+    for network in networks:
+
+        #extract network fc
+        network_fc = all_sub_df[all_sub_df['network'] == network]
+
+        #extract only subject columns
+        network_fc = network_fc.drop(columns = ['roi','network'])
+        
+        #compute noise ceiling
+        noise_ceilings = compute_noise_ceiling(network_fc)
+        #add to all_sub_df
+        network_df[network] = noise_ceilings
+
+
+
+    #save
+    network_df.to_csv(f'{params.results_dir}/noise_ceilings/{group}_{atlas}_{summary_type}_network_noise_ceilings.csv', index=False)
+
+
+summary_type = 'fc'
+
+#define all rois and networks
+all_rois = []
+all_networks = []
+for roi in roi_labels['label']:
+    for hemi in params.hemis:
+        all_rois.append(f'{hemi}_{roi}')
+        all_networks.append(roi_labels[roi_labels['label'] == roi]['network'].values[0])
+
+for group in age_groups:
+    print(f'Preprocessing {group} {summary_type} data...')
+    preprocess_data(group, summary_type, all_rois, all_networks)
+
+
+summary_type = 'cross_hemi_fc'
+
+#define all rois and networks
+all_rois = []
+all_networks = []
+for roi in roi_labels['label']:
+
+    all_rois.append(f'{roi}')
+    all_networks.append(roi_labels[roi_labels['label'] == roi]['network'].values[0])
+
+for group in age_groups:
+    print(f'Preprocessing {group} {summary_type} data...')
+    preprocess_data(group, summary_type, all_rois, all_networks)
