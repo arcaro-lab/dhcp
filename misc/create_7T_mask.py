7712df85946c1ce2b395fbf8e3011fd425902498
vayzenb <vayzenberg90@gmail.com>
2024-11-11 20:46:34 -0500

    added Chords

--- misc/create_7T_mask.py
+++ misc/create_7T_mask.py
@@ -3,10 +3,13 @@ Create 7T brain mask
 '''
 
 
-git_dir = '/mnt/c/Users/ArcaroLab/Desktop/git_repos/dhcp'
-
+project_name = 'dhcp'
 import os
+#get current working directory
+cwd = os.getcwd()
+git_dir = cwd.split(project_name)[0] + project_name
 import sys
+
 #add git_dir to path
 sys.path.append(git_dir)
 
@@ -15,15 +18,18 @@ import dhcp_params as params
 import pandas as pd
 
 
-raw_data_dir, raw_anat_dir, raw_func_dir, out_dir, anat_suf, func_suf, brain_mask_suf, group_template,template_name = params.load_group_params('adult')
+group_info = params.load_group_params('adult')
+out_dir = group_info.out_dir
+anat_suf = group_info.anat_suf
 
 #load participant file
-sub_file = pd.read_csv(f'{out_dir}/participants.csv')
+sub_file = pd.read_csv(f'{git_dir}/participants_7T.csv')
 
 #convert Atlas_wmparc.1.60.nii.gz to brain mask
 source_file_dir = f'MNINonLinear/ROIs/Atlas_wmparc.1.60.nii.gz'
 
 for sub in sub_file['participant_id']:
+    print(sub)
     sub_dir = f'{out_dir}/{sub}/ses-01'
 
     target_file =  f'{sub_dir}/anat/{sub}_ses-01_{anat_suf}_mask.nii.gz'
